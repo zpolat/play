d430a198ec29a31ca2e03c4b6a994b94c8d317a1
Zafer Polat <zafer@zafers-mm.local>
2023-02-18 15:04:20 +0100

    examples

--- test/example.spec.ts
+++ test/example.spec.ts
@@ -14,3 +14,22 @@ test('clicking on elements', async ({ page }) => {
   const errorMessage = page.locator('.alert-error')
   await expect(errorMessage).toContainText('Login and/or password are wrong.')
 })
+
+test('Selectors', async ({ page }) => {
+  //text
+  await page.click('text=some text')
+
+  //css selectors
+  await page.click('button')
+  await page.click('#id')
+  await page.click('.class')
+
+  //visible css selector
+  await page.click('submit-button:visible')
+
+  //combinations
+  await page.click('#username .first')
+  
+  //xpath 
+  await page.click('//button')
+})
